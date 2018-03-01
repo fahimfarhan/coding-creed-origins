0b5040bbe1176341d665108fe0df46591bee6cd6
fahimfarhan <fahim.farhan@outlook.com>
2018-03-01 18:48:51 +0600

    FC 4

--- prototype/temp2.cpp
+++ prototype/temp2.cpp
@@ -1,25 +1,18 @@
-#include<bits/stdc++.h>
-    
-using namespace std ;
-    
-void swa ( int x ){
-    cout<<"Debug x = "<<x<<endl;
-    if ( x == 1 ){
-    
-        cout << "Finish\n\n" ;
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int word,len,i;
+    char s[100000];
+    while(scanf("%[^\n]%*c",s)==1)
+    {
+        word=0;
+        len=strlen(s);
+        for(i=0;i<len;i++)
+        {
+            if((s[i+1]<'A'||(s[i+1]>'Z'&&s[i+1]<'a')||s[i+1]>'z')&&((s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z')))
+                word++;
+        }
+        printf("%d\n",word);
     }
-    
-    else{
-    //x--;
-        swa ( x-- ) ;
-    
-        cout << "X\t=\t" << x << endl << endl ;
-    }
-}
-    
-int main(){
-    
-    int a = 3 ;
-    
-    swa ( 3 ) ;
 }
