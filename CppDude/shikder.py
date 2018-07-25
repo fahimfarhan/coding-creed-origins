1f2b43ce834645c34138ba8510ac53d9542ed7b8
fahimfarhan <fahim.farhan@outlook.com>
2018-07-25 20:46:31 +0600

    FC 12

--- CppDude/shikder.py
+++ CppDude/shikder.py
@@ -1,5 +1,5 @@
 import requests
-
+import time
 def get_validate(email):
     # email = email.lower()
     # print("inside get_validate: "+str(email))
@@ -13,19 +13,97 @@ def get_validate(email):
 
 
 
-sites = ['bwcigroup.com','envirotectltd.co.uk',
-'allplantsales.com',
-'sabaleather.co.uk',
-'sensor-technik.co.uk',
-'oneonline.co.uk',
-'pact-engineering.co.uk',
-'www.uniquemployment.co.uk']
+sites = ['support@laautoservices.co.uk',
+'support@leestanley.net',
+'support@newkitchens.ltd.uk',
+'support@nfea.com',
+'support@polaris-laser.com',
+'support@turbosport.net',
+'support@vpress.co.uk',
+'support@wastego.com',
+'support@avondaleguesthouse.com',
+'support@beckfoot.co.uk',
+'support@ca1.co.uk',
+'support@cumbriayfc.co.uk',
+'support@darkseaweb.com',
+'support@gincase.co.uk',
+'support@golfhotelsilloth.co.uk',
+'support@hutton-in-the-forest.co.uk',
+'support@lovelady.co.uk',
+'support@lowther.co.uk',
+'support@palmyra-ltd.com',
+'support@pattersonheggie.co.uk',
+'support@quattro.org.uk',
+'support@rathbonesofkeswick.co.uk',
+'support@rosecottageguest.co.uk',
+'support@sf-property.co.uk',
+'support@trouthotel.co.uk',
+'support@wcda.co.uk',
+'support@achilles.com',
+'support@adpads.co.uk',
+'support@aegisengineering.co.uk',
+'support@antibo.co.uk',
+'support@auto-supplies.co.uk',
+'support@belpernaturalhealth.co.uk',
+'support@castle-cliffe.com',
+'support@castlehomesderby.co.uk',
+'support@charapak.co.uk',
+'support@clean-slate.biz',
+'support@cnainternational.co.uk',
+'support@critical-skills.co.uk',
+'support@darleys.com',
+'support@darling-buds.com',
+'support@daubneyagency.co.uk',
+'support@dave-griffin.co.uk',
+'support@dcil.org.uk',
+'support@derbypcrepairs.com',
+'support@derbyshirecountyshow.org.uk',
+'support@derbyshireforklifts.co.uk',
+'support@directpumpsandtanks.co.uk',
+'support@dpsonline.org.uk',
+'support@easibind.com',
+'support@fireworksinternational.co.uk',
+'support@flexi-print.co.uk',
+'support@gatesuk.com',
+'support@hallampolymer.com',
+'support@holroyd-instruments.com',
+'support@ics-ltd.co.uk',
+'support@neway-training.com',
+'support@norbury.derbyshire.sch.uk'
+ ]
 
-begin = ['info', 'hello', 'support', 'admin', 'sales', 'account']
-
-
+begin = ['hello']
+global count
+count = 0
+st = time.time()
+'''
 for i in begin:
-    for j in sites:
-        mail_prototype = str(i)+'@' + str(j)
+    try:
+        for j in sites:
+            try:
+                count = count + 1
+                mail_prototype = str(i) + str(j)
+                check = get_validate(mail_prototype)
+                print(str(count)+" "+str(mail_prototype)+" : "+str(check))
+            except Exception as x:
+                print("\n---"+str(x)+"---\n")
+                pass 
+        print("\n\n-------------------------------\n\n")
+    except:
+        pass
+end = time.time()
+diff = end - st
+print(str(diff)+" sec\n")
+'''
+
+for j in sites:
+    try:
+        mail_prototype = str(j)
         check = get_validate(mail_prototype)
         print(str(mail_prototype)+" : "+str(check))
+    except:
+        pass
+
+end = time.time()
+diff = end - st
+print(str(diff)+" sec\n")
