bd336af51a6a2ef3d9f46ed84e80008a0011acd6
fahimfarhan <fahim.farhan@outlook.com>
2019-04-09 20:51:25 +0600

    FC auto-commit

--- 0-Programming_Contest_DS_Algorithms/nCr.cpp
+++ 0-Programming_Contest_DS_Algorithms/nCr.cpp
@@ -2,16 +2,18 @@
 
 using namespace std;
 
+#define ll long long int
+
 class nCr{
 private:
-    int n,r;
-    int **dp;
+    ll n,r;
+    ll **dp;
 public:
     int getDpIJ(int i, int j){ return dp[i][j];   }
     nCr(int N, int R){
         n=N; r=R;
-        dp = new int*[n+1];
-        for(int i=0; i<=n;i++){ dp[i] = new int[r+1]; }
+        dp = new ll*[n+1];
+        for(int i=0; i<=n;i++){ dp[i] = new ll[r+1]; }
         dp[0][0] = 1;
         /*for(int i=0; i<=n; i++){
             dp[i][0] = 1;
@@ -26,13 +28,14 @@ public:
                 else dp[i][j] = dp[i-1][j-1]+dp[i-1][j]; // nCr = n-1Cr-1 + n-1Cr
             }
         }
-        
+    }
+
+    void printer(){
         for(int i=0; i<=n; i++){
             for(int j=0; j<=r; j++){
                 cout<<dp[i][j]<<" ";
             }cout<<"\n";
         }
-
     }
     ~nCr(){
         for(int i=0; i<=n;i++)
@@ -46,6 +49,11 @@ int main()
     int n,r;
     cout<<"Enter n, r: ";
     cin>>n>>r;
+    if(r>n){
+        cout<<"INVALID INPUT! r>n \n";
+        exit(0);
+    }
     nCr ncr(n,r);
+    ncr.printer();
     return 0;
 }
