9652e5af9165feb493c5311eb4c7ca83b77d40e2
Soumic <fahim.farhan@outlook.com>
2020-12-08 14:56:01 +0600

    :rocket: auto-commit

---
+++ algos/c3primeInterval.cpp
@@ -0,0 +1,137 @@
+/*************************************************************************
+  SPDX-License-Identifier: MIT
+  Copyright (c) 2020 Qazi Fahim Farhan (@fahimfarhan)
+  May the CodeForces be with you!
+ ************************************************************************/
+/**
+// ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶  ⠄⠄⠄⠄⠄⠄⢴⡶⣶⣶⣶⡒⣶⣶⣖⠢⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄  
+// ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶  ⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿  ⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆
+// ⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿  ⠇⢀⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿  ⣿⣿⠃⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇
+*/
+
+#include <iostream>
+#include <climits>  // this includes INT_MIN, INT_MAX, ... ...
+// #include <sstream>
+// #include <cstdio>
+// #include <cmath>
+// #include <cstring>
+// #include <cctype>
+// #include <string>
+#include <vector>
+// #include <list>
+// #include <set>
+// #include <unordered_set>
+// #include <map>
+// #include <unordered_map>
+// #include <queue>
+// #include <stack>
+#include <algorithm>
+// #include <functional>
+#include <iomanip>      // std::setprecision
+
+
+using namespace std;
+
+#define PI 2*acos(0)
+
+//typedef long long int ll;
+ #define ll long long int
+// other popular ones=> int64_t, uint64_t => use for 10^18
+ll MOD = 1e9+7;
+
+// int n,m;
+vector<int> *g;
+bool *isvisited;
+
+void start() {}
+
+void FastIO() {
+  // freopen("input.txt","r",stdin);
+  // freopen("output.txt","w",stdout);
+  /*
+  std::cout << std::fixed;
+  std::cout << std::setprecision(10);
+  cout << num1 << endl;
+  */
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  /*
+---------- Interactive problems ---------
+
+  on each interactive questions' end, add `cout.flush()`
+  say, cout<<"some weirdo question";
+  cout<<"\n"; // end of question
+  cout.flush(); // <-- just like this
+
+  if still confusing, check out 1363D.cpp
+    
+*/
+}
+
+#define N 10001
+bool isNonPrime[N+1]; // by default false. reinit will only kill time
+vector<int> primes;
+
+inline void seivePreprocess() {
+  isNonPrime[0] = true;
+  isNonPrime[1] = true;
+
+  for(int i=4; i<=N; i+=2) {
+    isNonPrime[i] = true;
+  }
+
+  for(int i=3; i*i<=N; i+=2) {
+    if(isNonPrime[i] == false) {
+      for(int j=i*i; j<=N; j+=i) {
+        isNonPrime[j] = true;
+      } 
+    }
+  }
+
+  for(int i=0; i<=N; i++) {
+    if(isNonPrime[i] == false) {
+      primes.push_back(i);
+    }
+  }
+}
+
+int main(int argc, char const *argv[]){
+  /* code */
+  FastIO();
+
+  seivePreprocess();
+
+  int T, L, R;
+  // cin>>T;
+  T = 1;
+  vector<int>::iterator lb;
+  vector<int>::iterator ub;
+  while(T--) {
+    cin>>L>>R;
+
+    lb = lower_bound(primes.begin(), primes.end(), L);
+    ub = upper_bound(primes.begin(), primes.end(), R);
+
+    // cout<<*lb<<"\n";
+    // cout<<*ub<<"\n";
+    for(auto it = lb; it != ub; it++) {
+      cout<<(*it)<<" ";
+    }cout<<"\n";
+  }
+
+  return 0;
+}
---
+++ algos/c4-macro-and-prime-prime.cpp
@@ -0,0 +1,141 @@
+/*************************************************************************
+  SPDX-License-Identifier: MIT
+  Copyright (c) 2020 Qazi Fahim Farhan (@fahimfarhan)
+  May the CodeForces be with you!
+ ************************************************************************/
+/**
+// ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶  ⠄⠄⠄⠄⠄⠄⢴⡶⣶⣶⣶⡒⣶⣶⣖⠢⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄  
+// ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶  ⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿  ⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆
+// ⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿  ⠇⢀⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿  ⣿⣿⠃⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇
+*/
+
+#include <iostream>
+#include <climits>  // this includes INT_MIN, INT_MAX, ... ...
+// #include <sstream>
+// #include <cstdio>
+// #include <cmath>
+// #include <cstring>
+// #include <cctype>
+// #include <string>
+#include <vector>
+// #include <list>
+// #include <set>
+// #include <unordered_set>
+// #include <map>
+// #include <unordered_map>
+// #include <queue>
+// #include <stack>
+#include <algorithm>
+// #include <functional>
+#include <iomanip>      // std::setprecision
+
+
+using namespace std;
+
+#define PI 2*acos(0)
+
+//typedef long long int ll;
+ #define ll long long int
+// other popular ones=> int64_t, uint64_t => use for 10^18
+ll MOD = 1e9+7;
+
+// int n,m;
+vector<int> *g;
+bool *isvisited;
+
+void start() {}
+
+void FastIO() {
+  // freopen("input.txt","r",stdin);
+  // freopen("output.txt","w",stdout);
+  /*
+  std::cout << std::fixed;
+  std::cout << std::setprecision(10);
+  cout << num1 << endl;
+  */
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  /*
+---------- Interactive problems ---------
+
+  on each interactive questions' end, add `cout.flush()`
+  say, cout<<"some weirdo question";
+  cout<<"\n"; // end of question
+  cout.flush(); // <-- just like this
+
+  if still confusing, check out 1363D.cpp
+    
+*/
+}
+
+#define N 1000001
+bool isNonPrime[N+1]; // by default srt to false. do not reinit as it kills time
+// vector<int> primes;
+int primePrimes[N+1];
+
+inline void preprocess() {
+  isNonPrime[0] = true;
+  isNonPrime[1] = true;
+
+  for(int i=4; i<=N; i+=2) {  isNonPrime[i] = true; }
+
+  for(int i=3; i*i <=N; i+=2) {
+    if(isNonPrime[i] == false) {
+      for(int j=i*i; j<=N; j+=i) {
+        isNonPrime[j] = true;
+      }
+    }
+  }
+
+  int kount = 0;
+  for(int i=0; i<N; i++) {
+    if(isNonPrime[i] == false) {
+      // primes.push_back(i);
+      kount++;
+    }
+    if(isNonPrime[kount] == false)
+      primePrimes[i] = 1;
+    else primePrimes[i] = 0;
+  }
+
+  for(int i=1; i<N; i++) {
+    primePrimes[i] += primePrimes[i-1];
+  }
+
+
+
+}
+
+int main(int argc, char const *argv[]){
+  /* code */
+  FastIO();
+  preprocess();
+
+  int T, L, R;
+  cin>>T;
+  while(T--) {
+    cin>>L>>R;
+
+    int kount = 0;
+    kount = primePrimes[R]-primePrimes[L-1];
+    // for(int i=L; i<=R; i++) {
+    //   kount+=primePrimes[i];
+    // }
+    cout<<kount<<"\n";
+  }
+  return 0;
+}
