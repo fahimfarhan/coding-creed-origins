b95aeb203b0fa19735a082455520b9f6ad2a3524
fahimfarhan <fahim.farhan@outlook.com>
2021-04-24 01:02:00 +0600

    :rocket: auto-commit

---
+++ Codes/CodeForces/B/1516B.cpp
@@ -0,0 +1,209 @@
+/*************************************************************************
+  SPDX-License-Identifier: MIT
+  Copyright (c) 2020 Qazi Fahim Farhan (@fahimfarhan)
+  May the CodeForces be with you!
+ ************************************************************************/
+/**
+// ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶  ⠄⠄⠄⠄⠄⠄⢴⡶⣶⣶⣶⡒⣶⣶⣖⠢⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄  
+// ⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶  ⠄⠄⠄⠄⠄⠄⢠⣿⣋⣿⣿⣉⣿⣿⣯⣧⡰⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⣿⣿⣹⣿⣿⣏⣿⣿⡗⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠟⡛⣉⣭⣭⣭⠌⠛⡻⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⣤⡌⣿⣷⣯⣭⣿⡆⣈⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿  ⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣷⢛⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢻⣷⣽⣿⣿⣿⢿⠃⣼⣧⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣛⣻⣿⠟⣀⡜⣻⢿⣿⣿⣶⣤⡀⠄⠄⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢠⣤⣀⣨⣥⣾⢟⣧⣿⠸⣿⣿⣿⣿⣿⣤⡀⠄⠄⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⠄⢟⣫⣯⡻⣋⣵⣟⡼⣛⠴⣫⣭⣽⣿⣷⣭⡻⣦⡀⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⢏⣽⣿⢋⣾⡟⢺⣿⣿⣿⣿⣿⣿⣷⢹⣷⠄
+// ⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢣⣿⣿⣿⢸⣿⡇⣾⣿⠏⠉⣿⣿⣿⡇⣿⣿⡆
+// ⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿  ⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⢸⣿⣿⣿⠸⣿⡇⣿⣿⡆⣼⣿⣿⣿⡇⣿⣿⡇
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿  ⠇⢀⠄⠄⠄⠄⠄⠘⣿⣿⡘⣿⣿⣷⢀⣿⣷⣿⣿⡿⠿⢿⣿⣿⡇⣩⣿⡇
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿  ⣿⣿⠃⠄⠄⠄⠄⠄⠄⢻⣷⠙⠛⠋⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⡇⣿⣿⡇
+*/
+
+#include <iostream>
+#include <climits>  // this includes INT_MIN, INT_MAX, ... ...
+// #include <sstream>
+// #include <cstdio>
+// #include <cmath>
+// #include <cstring>
+// #include <cctype>
+// #include <string>
+#include <vector>
+// #include <list>
+// #include <set>
+// #include <unordered_set>
+// #include <map>
+// #include <unordered_map>
+// #include <queue>
+// #include <stack>
+#include <algorithm>
+// #include <functional>
+#include <iomanip>      // std::setprecision
+// #include <regex>   // for regular expressions
+
+using namespace std;
+
+#define PI 2*acos(0)
+
+//typedef long long int ll;
+ #define ll long long int
+// other popular ones=> int64_t, uint64_t => use for 10^18
+ll MOD = 1e9+7;
+
+// int n,m;
+vector<int> *g;
+bool *isvisited;
+
+#define GREATER 1
+#define EQUAL 0
+#define SMALLER -1
+
+int bigIntComparator(string a, string b) {
+  int sizeOfA = a.size();
+  int sizeOfB = b.size();
+  if(sizeOfA > sizeOfB) { return GREATER; }
+  else if(sizeOfA < sizeOfB) { return SMALLER; }
+  else{
+    for(int i=0; i<sizeOfA; i++) {
+      if(a[i]  < b[i]) {  return SMALLER; }
+      else if(a[i] > b[i]) {  return GREATER; }
+    }
+  }
+  return EQUAL;
+}
+
+
+string bigIntSum(string a, string b) {
+  string summation = "";
+
+  if(a.size() < b.size()) {
+    swap(a, b);
+  }
+
+  int len1 = a.size(), len2 = b.size();
+
+  int carry = 0;
+  char ch;
+  // int n = len2;
+  int x = 0;
+  for(int i=len1 - 1, j= len2-1; i>=0; i--, j--) {
+    if(j >= 0) {
+      x = (a[i] - '0') + (b[j] - '0') + carry;
+    }else{
+      x = (a[i] - '0') + carry;
+    }
+    if(x < 10) {  carry = 0;  }
+    else{ carry = 1;  }
+
+    x = x % 10;
+    ch = x + '0';
+    summation = ch + summation;
+  }
+
+  if(carry == 1) {
+    summation = "1" + summation;
+  }
+
+  return summation;
+}
+
+void start() {}
+
+void FastIO() {
+  // freopen("input.txt","r",stdin);
+  // freopen("output.txt","w",stdout);
+  /*
+  std::cout << std::fixed;
+  std::cout << std::setprecision(10);
+  cout << num1 << endl;
+  */
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  /*
+---------- Interactive problems ---------
+
+  on each interactive questions' end, add `cout.flush()`
+  say, cout<<"some weirdo question";
+  cout<<"\n"; // end of question
+  cout.flush(); // <-- just like this
+
+  if still confusing, check out 1363D.cpp
+    
+*/
+}
+
+inline bool isPossible(int *prefix, int *suffix, int N) {
+  int left, mid, right;
+
+  // bool isPossible = false;
+
+  for(int i=1; i<=N; i++) {
+    left = prefix[i];
+    for(int j=i+1; j<N; j++) {
+      int middle = prefix[i]^prefix[j];
+      int right = suffix[j+1];
+      if( (left == middle) && (middle == right) ) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+int main(int argc, char const *argv[]){
+  /* code */
+  FastIO();
+
+  int T, N;
+  int *a, *prefix, *suffix;
+
+  cin>>T;
+  while(T--) {
+    cin>>N;
+    a = new int[N+2];
+    prefix = new int [N+2];
+    suffix = new int[N+2];
+
+
+
+    for(int i=1; i<=N; i++) {
+      cin>>a[i];
+    }
+
+    a[0] = 0;
+    for(int i=1; i<=N; i++) {
+      a[0] = a[0]^a[i];
+    }
+
+    if(a[0] == 0) {
+      cout<<"YES\n";
+    }else{
+      // preprocess
+      prefix[0] = 0;
+    
+      for(int i=1; i<=N; i++) {
+        prefix[i] = prefix[i-1] ^ a[i];
+      }
+    
+      suffix[N+1] = 0;
+      for(int i=N; i>=1; i--) {
+        suffix[i] = a[i] ^ suffix[i+1];
+      }
+
+      if(isPossible(prefix, suffix, N)) {
+        cout<<"YES\n";
+      }else{
+        cout<<"NO\n";
+      }
+    }
+
+    delete[] suffix;
+    delete[] prefix;
+    delete[] a;
+  }
+
+  return 0;
+}
---
+++ Codes/CodeForces/D/118D.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+/*
+ 
+int caesarsLegions1(const int& n) {
+  if(n == 0) {
+    return 1;
+  } if(n == 1) {  return 1; }
+ 
+  return 1 + caesarsLegions1( (n-1) );
+}
+ 
+int caesarsLegion2(int n, int f, int h) {
+  if(n == 0) {
+    return 1;
+  }
+  if(n == 1) {
+    return 1;
+  }
+  // case horse
+  int x = 0;
+  if(h > 0) x = caesarsLegion2((n-1), f, (h-1));
+ 
+  // case infintry
+  int y = 0;
+  if(f > 0) y = caesarsLegion2((n-1), (f-1), h);
+  return (x+y);
+}
+ 
+int caesarsLegion3(const int& nf, const int& nh, const int& f, const int& h, const int& limitF, const int& limitH) {
+  // x = foot, y = horse
+  int x = 0; 
+  int y = 0;
+ 
+  if( (nf+nh) == 0) {  return 1; } 
+  
+  // foot
+  if( (nf > 0) && (f > 0) ) {
+    x = caesarsLegion3( (nf-1), nh, (f-1), limitH, limitF, limitH);
+  }
+ 
+  // horse
+  if( (nh > 0) && (h > 0) ) {
+    y = caesarsLegion3( nf, (nh - 1), limitF, (h-1) , limitF, limitH);
+  }
+ 
+  return (x + y);
+}
+ 
+*/
+ 
+ 
+#define MOD 100000000
+ 
+ 
+ 
+int ****dp;
+ 
+inline void initDp(const int& n1, const int& n2, const int& k1, const int& k2) {
+  dp = new int***[n1+1];
+  for(int i=0; i<=n1; i++) {
+    dp[i] = new int**[n2+1];
+    for(int j=0; j<= n2; j++) {
+      dp[i][j] = new int*[k1+1];
+      for(int k = 0; k <= k1; k++) {
+        dp[i][j][k] = new int[k2+1];
+      }
+    }
+  }
+ 
+  for(int i=0; i<=n1; i++) {
+    for(int j=0; j<= n2; j++) {
+      for(int k = 0; k <= k1; k++) {
+        for(int l=0; l <= k2; l++) {
+          if( (i|j) == 0 ) {
+            dp[i][j][k][l] = 1;
+          }else{
+            dp[i][j][k][l] = -1;
+          }
+        }
+      }
+    }
+  }
+}
+ 
+inline void destructor(const int& n1, const int& n2, const int& k1, const int& k2) {
+  for(int i=0; i<=n1; i++) {
+    for(int j=0; j<= n2; j++) {
+      for(int k = 0; k <= k1; k++) {
+        delete[] dp[i][j][k]; //  = new int[k2+1];
+      }
+      delete[] dp[i][j]; // = new int*[k1+1];
+    }
+    delete[] dp[i]; // = new int**[n2+1];
+  }
+  delete[] dp;
+}
+ 
+inline int caesarsLegion(const int& nf, const int& nh, const int& f, const int& h, const int& limitF, const int& limitH) {
+  if(dp[nf][nh][f][h] != -1) {
+    return dp[nf][nh][f][h];
+  }
+  
+  // X = foot
+  int x = 0;
+  if( (nf > 0) && (f > 0) ) {
+    x = dp[(nf - 1)][nh][(f-1)][limitH];
+    if(x == -1) {
+      dp[(nf - 1)][nh][(f-1)][limitH] = caesarsLegion((nf-1), nh, (f-1), limitH, limitF, limitH);
+      x = dp[(nf - 1)][nh][(f-1)][limitH];
+    }
+  }
+  // Y = horse
+  int y = 0; 
+  if( (nh > 0) && (h>0) ) {
+    y = dp[nf][(nh-1)][limitF][(h-1)];
+    if(y == -1) {
+      dp[nf][(nh-1)][limitF][(h-1)] = caesarsLegion(nf, (nh-1), limitF, (h-1), limitF, limitH);
+      y = dp[nf][(nh-1)][limitF][(h-1)];
+    }
+  }
+ 
+  dp[nf][nh][f][h] = (x + y) % MOD;
+  return dp[nf][nh][f][h];
+}
+ 
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+ 
+  int n1, n2, k1, k2;
+  cin>>n1>>n2>>k1>>k2;
+ 
+  // step 1
+  //int step1 = caesarsLegions1((n1+n2));
+  // step 2
+  //int step2 = caesarsLegion2((n1+n2), k1, k2);
+  // step 3
+  //int step3 = caesarsLegion3( n1, n2, k1, k2, k1, k2);
+  //cout<<step3<<"\n"; 
+ 
+  initDp(n1, n2, k1, k2); 
+ 
+  int step4 = caesarsLegion(n1, n2, k1, k2, k1, k2);
+  cout<<step4<<"\n";
+ 
+  destructor(n1, n2, k1, k2); 
+  return 0;
+}
